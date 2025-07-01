fa60a68c7796fef434bf5c6ffdabd27204d51e7b
jd9.snflwr <ok.neutrino@proton.me>
2025-07-01 22:14:17 +1200

    Deviation

--- .github/copilot-instructions.md
+++ .github/copilot-instructions.md
@@ -2,7 +2,7 @@
 - Project name: TaswellGame
 - Unity version: Unity 6000.0.31f1
 - Active game object:
-  - Name: BACKGROUND
+  - Name: ELEV_DOORS
   - Tag: Untagged
   - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/Scripts/ElevScript.cs
+++ Assets/Scripts/ElevScript.cs
@@ -10,45 +10,43 @@ public class ElevScript : MonoBehaviour
     public SceneManager sceneManager;
     public bool hasPassenger;
 
+    public bool isLitirallyClosed;
+
     private void Update()
     {
-        //Check if prefab is Entrance or Exit
+        if (this.anim.GetCurrentAnimatorStateInfo(0).IsName("Elev_Closed"))
+        {
+            isLitirallyClosed = true;
+            playerDetection.enabled = false;
+        }
+
+        if (this.anim.GetCurrentAnimatorStateInfo(0).IsName("Elev_Open"))
+        {
+            isLitirallyClosed = false;
+            playerDetection.enabled = true;
+        }
+
+        if (hasPassenger)
+        {
+            self.transform.position = new Vector3(0, 0, -3);
+        }
+        else { self.transform.position = new Vector3(0, 0, 0); }
+
         if (isExit)
         {
             if (sceneManager.exitElevatorIsCharged)
             {
                 anim.SetBool("IsClosed", false);
-                transform.position = new Vector3(transform.position.x, transform.position.y, 0f);
             }
-            else if (!sceneManager.exitElevatorIsCharged && !hasPassenger)
-            {
-                anim.SetBool("IsClosed", true);
-                transform.position = new Vector3(transform.position.x, transform.position.y, 0f);
-            }
-            else if (!sceneManager.exitElevatorIsCharged && hasPassenger)
-            {
-                anim.SetBool("IsClosed", true);
-                transform.position = new Vector3(transform.position.x, transform.position.y, -2f);
-            }
+            else { anim.SetBool("IsClosed", true); }
         }
-
         if (!isExit)
         {
             if (sceneManager.ElevatorIsCharged)
             {
                 anim.SetBool("IsClosed", false);
-                transform.position = new Vector3(transform.position.x, transform.position.y, 0f);
             }
-            else if (!sceneManager.ElevatorIsCharged && !hasPassenger)
-            {
-                anim.SetBool("IsClosed", true);
-                transform.position = new Vector3(transform.position.x, transform.position.y, 0f);
-            }
-            else if (!sceneManager.exitElevatorIsCharged && hasPassenger)
-            {
-                anim.SetBool("IsClosed", true);
-                transform.position = new Vector3(transform.position.x, transform.position.y, -2f);
-            }
+            else { anim.SetBool("IsClosed", true); }
         }
     }
 
